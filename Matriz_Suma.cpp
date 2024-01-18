39f2160d5401fcd164e57f58c33094da0a99754e
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-18 12:41:19 -0600

    Matriz_Suma.cpp

--- Matriz_Suma.cpp
+++ Matriz_Suma.cpp
@@ -1,47 +1,47 @@
 /*
 				***** SUMA DE VALORES DE LA MATRIZ *****
 	
-		Descripci髇:
+		Descripci贸n:
 		El siguiente programa implemnta la suma de los valores de una matriz partiendo por los datos dados
 		por el usuario, imprimiendo en consola el resultado y la matriz resultante.
 		Fecha: 16-Abril-2021	
-		Versi髇: 1.0
+		Versi贸n: 1.0
 */
 
-#include <iostream>																					// Biblioteca estandar de entrada y salida de datos
+#include <iostream>															// Biblioteca estandar de entrada y salida de datos
 
-using namespace std;																				// Acceso a todas las funciones 
+using namespace std;															// Acceso a todas las funciones 
 
-main()																								// Funci髇 principal
+main()																	// Funci贸n principal
 {
 	
-	int matriz[4][2];																				// Declaraci髇 de la matriz
-	int suma=0;																						// Declaraci髇 e inicializaci髇 de un acumulador
-	system("color f3");																				// Establece el color de fondo en blanco claro
+	int matriz[4][2];														// Declaraci贸n de la matriz
+	int suma=0;															// Declaraci贸n e inicializaci贸n de un acumulador
+	system("color f3");														// Establece el color de fondo en blanco claro
 		
-	for(int f=0; f<4; f++)																			// Ciclo for de cuatro iteraciones correspondientes a las posiciones de la fila
+	for(int f=0; f<4; f++)														// Ciclo for de cuatro iteraciones correspondientes a las posiciones de la fila
 	{
-		for(int c=0; c<2; c++)																		// Ciclo for de dos iteraciones correspondientes a las posiciones de la columna
+		for(int c=0; c<2; c++)													// Ciclo for de dos iteraciones correspondientes a las posiciones de la columna
 		{
 			cout<<"Ingresa el valor de la posicion ["<<f+1<<"]["<<c+1<<"]: ";						// Imprime texto en consola solicitando el valor correspondiente
-			cin>>matriz[f][c];																		// Asigna el valor ingresado con respecto a las iteraciones de f y c
-		    suma=suma+	matriz[f][c];																// Acumula la suma de los valores de los campos de la matriz
+			cin>>matriz[f][c];												// Asigna el valor ingresado con respecto a las iteraciones de f y c
+		    suma=suma+	matriz[f][c];												// Acumula la suma de los valores de los campos de la matriz
 		}
 	}
 	
-	cout<<"\n\n\n";																					// Agrega tres saltos de linea
+	cout<<"\n\n\n";															// Agrega tres saltos de linea
  	cout<<"\t\t\t\tLa suma de los numeros de la matriz es: "<<suma;									// Imprime texto en consola de resultado
-	cout<<"\n\n\t\t\t\t";																			// Agrega dos saltos de linea junto con tres tabulaciones
+	cout<<"\n\n\t\t\t\t";														// Agrega dos saltos de linea junto con tres tabulaciones
 	
-	for(int f=0; f<4; f++)																			// Ciclo for de cuatro iteraciones correspondientes a las posiciones de la fila
+	for(int f=0; f<4; f++)														// Ciclo for de cuatro iteraciones correspondientes a las posiciones de la fila
 	{
-		for(int c=0; c<2; c++)																			// Ciclo for de dos iteraciones correspondientes a las posiciones de la columna
+		for(int c=0; c<2; c++)													// Ciclo for de dos iteraciones correspondientes a las posiciones de la columna
 		{
 			
-			cout<<"\t| "<<matriz[f][c];																// Imprime en consola la matriz con los valores en su posici髇
+			cout<<"\t| "<<matriz[f][c];											// Imprime en consola la matriz con los valores en su posici贸n
 		}
-		cout<<"\n\t\t\t\t";																			// Agrega un salto de linea junto con tres tabulaciones
+		cout<<"\n\t\t\t\t";													// Agrega un salto de linea junto con tres tabulaciones
 	}
 	
-	return 0;																						// Programa finalizado con 閤ito
+	return 0;															// Programa finalizado con 茅xito
 }
