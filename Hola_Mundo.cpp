3fa588c4b8bfae5dda9dfd78e8ff3d9a8ec7d26a
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-18 12:18:54 -0600

    Hola_Mundo.cpp

--- Hola_Mundo.cpp
+++ Hola_Mundo.cpp
@@ -1,24 +1,24 @@
 /*
 				***** HOLA MUNDO *****
 				
-		Descripción:
-		El presente programa presenta la implementación del hola mundo
+		DescripciÃ³n:
+		El presente programa presenta la implementaciÃ³n del hola mundo
 		desarrollado en C++.
 	 	Version: 1.0					  
 		Fecha: 02-Febrero-2021		 
 */
 
-#include <iostream>								// LIBRERIA PARA ENTRADA Y SALIDA DE LOS DATOS
-	
-using namespace std;							// ACCESO A TODAS LAS FUNCIONES
+#include <iostream>									// LIBRERIA PARA ENTRADA Y SALIDA DE LOS DATOS
+		
+using namespace std;									// ACCESO A TODAS LAS FUNCIONES
 
 // cout<<										// IMPRIME EL DATO QUE SE REQUIERE
 
-main ()											// FUNCIÓN PRINCIPAL
+main ()											// FUNCIÃ“N PRINCIPAL
 {
-	cout<<"\n\n\n";								// AGREGA TRES SALTOS DE LINEA ANTES DEL TEXTO
-	cout<<"\t\t\t HOLA MUNDO";				    // AGREGA TABULACIONES AL TEXTO
-	cout<<"\n\n\n";								// AGREGA TRES SALTOS DE LINEA DESPUES DEL TEXTO
+	cout<<"\n\n\n";									// AGREGA TRES SALTOS DE LINEA ANTES DEL TEXTO
+	cout<<"\t\t\t HOLA MUNDO";		  				        // AGREGA TABULACIONES AL TEXTO
+	cout<<"\n\n\n";								        // AGREGA TRES SALTOS DE LINEA DESPUES DEL TEXTO
 	
 	return 0;									// INDICA EL FIN DEL PROGRAMA
 }
