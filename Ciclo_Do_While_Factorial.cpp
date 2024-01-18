046234b95eba5df92a7fb226d518b0f1bb2b2280
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-18 12:35:24 -0600

    Ciclo_Do_While_Factorial.cpp

--- Ciclo_Do_While_Factorial.cpp
+++ Ciclo_Do_While_Factorial.cpp
@@ -1,42 +1,42 @@
 /*
 					***** CICLO DO WHILE *****
 		
-		Descripci髇:		
-		EL siguiente programa calcula el factorial de un n鷐ero dado por el usuario y pregunta si requiere 
-		calcular otro n鷐ero, implementando el ciclo do while.
+		Descripci贸n:		
+		EL siguiente programa calcula el factorial de un n煤mero dado por el usuario y pregunta si requiere 
+		calcular otro n煤mero, implementando el ciclo do while.
 		Fecha: 10-Marzo-2021
-		Versi髇: 1.0
+		Versi贸n: 1.0
 		
 */
-#include <iostream>																						// Biblioteca estandar de entrada y salida de datos
+#include <iostream>															// Biblioteca estandar de entrada y salida de datos
 
-using namespace std;																					// Acceso a todas las funciones
+using namespace std;															// Acceso a todas las funciones
 
-main()																									// Funci髇 principal
+main()																	// Funci贸n principal
 {
  
-  int i, numero, factor;																				// Definici髇 de variables para el ciclo, el numero a calcular y el resultado correspondiente
-  system("color f3");																					// Establece el color de fondo blanco claro
-  char op;																								// Define la variable de opci髇 en tipo char
+  int i, numero, factor;														// Definici贸n de variables para el ciclo, el numero a calcular y el resultado correspondiente
+  system("color f3");															// Establece el color de fondo blanco claro
+  char op;																// Define la variable de opci贸n en tipo char
   
-  do																									// Inicializa el ciclo do
+  do																	// Inicializa el ciclo do
   {
-  	cout<<"Ingresa un numero entero: ";																	// Imprime el texto pidiendo el numero para calcular
-  		cin>>numero;																					// Asigna el valor a la variable numero
-  	factor=1;																							// Inicializa el valor 1 a la variable factor
-  		i=1;																							// Inicializa el valor 1 a la variable i
-  while(i<=numero)																						// Inicio del ciclo while que se ejecuta mientras la variable i sea menor al numero
+  	cout<<"Ingresa un numero entero: ";												// Imprime el texto pidiendo el numero para calcular
+  		cin>>numero;														// Asigna el valor a la variable numero
+  	factor=1;															// Inicializa el valor 1 a la variable factor
+  		i=1;															// Inicializa el valor 1 a la variable i
+  while(i<=numero)															// Inicio del ciclo while que se ejecuta mientras la variable i sea menor al numero
   {
-  	factor= factor*i;																					// Ejecuta el c醠culo
-   i++;																									// Incrementa el valor de i
+  	factor= factor*i;														// Ejecuta el c谩lculo
+   i++;																	// Incrementa el valor de i
   }
-  cout<<"El factorial del numero "<<numero<<" es: "<<factor;											// Imprime texto en consola con el n鷐ero y su factorial correspondiente
+  cout<<"El factorial del numero "<<numero<<" es: "<<factor;										// Imprime texto en consola con el n煤mero y su factorial correspondiente
   cout<<"\n\n";																							// Agrega dos saltos de linea
-  cout<<"Deseas calcular otro facotorial S para si / o N para no: ";									// Imprime texto en consola solicitando si se requiere calcular otro n鷐ero
-  cin>>op;																								// Asigna el valor a la variable op
+  cout<<"Deseas calcular otro facotorial S para si / o N para no: ";									// Imprime texto en consola solicitando si se requiere calcular otro n煤mero
+  cin>>op;																// Asigna el valor a la variable op
   }
   
-  while (op=='s' || op=='S');																			// Toma desici髇 si entra de nuevo el ciclo
+  while (op=='s' || op=='S');														// Toma desici贸n si entra de nuevo el ciclo
  
  	return 0;
  }
