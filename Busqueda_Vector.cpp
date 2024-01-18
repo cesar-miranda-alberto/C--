6dce3a6ce93caf58de41d546aa9b10a75c7e8885
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-18 12:23:09 -0600

    Busqueda_Vector.cpp

--- Busqueda_Vector.cpp
+++ Busqueda_Vector.cpp
@@ -1,43 +1,43 @@
 /*
 				***** BUSQUEDA EN VECTOR *****
 				
-		Descripci髇:
-		El siguiente c骴igo tiene como finalidad implementar la busqueda de un numero dado por el usuario
+		Descripci贸n:
+		El siguiente c贸digo tiene como finalidad implementar la busqueda de un numero dado por el usuario
 		dentro de un vector con valores inicializados.
 */
 
 #include <iostream>																				// Biblioteca estandar de entrada y salida de datos
 
-using namespace std;																			// Acceso a las funciones
+using namespace std;																			        // Acceso a las funciones
 
-main()																							// Funci髇 principal
+main()																					        // Funci贸n principal
 {
 
    
-   int vector[]={14,3,6,9,21,56,89,0,1,45};															// Vector inicializado
-   int num, b=0;																				// Declaraci髇 de variablie e inicializaci髇
-   system("color f3");																			// Define color de fondo en blanco claro
+   int vector[]={14,3,6,9,21,56,89,0,1,45};																	// Vector inicializado
+   int num, b=0;																				// Declaraci贸n de variablie e inicializaci贸n
+   system("color f3");																				// Define color de fondo en blanco claro
    
-   cout<<"Ingresa un numero entero: ";															// Imprime texto en consola pidiendo numero
+   cout<<"Ingresa un numero entero: ";																		// Imprime texto en consola pidiendo numero
    cin>>num;																					// Asigna el valor a la variable num															
    
-   for(int i=0; i<=10; i++)																		// Ciclo for de diez iteraciones correspondientes a los espacios del vector
+   for(int i=0; i<=10; i++)																			// Ciclo for de diez iteraciones correspondientes a los espacios del vector
    {
    	
-   		if(num==vector[i])																		// Condicional si se encuetra el numero ingresado en la posici髇 de iteraci髇 del vector
+   		if(num==vector[i])																		// Condicional si se encuetra el numero ingresado en la posici贸n de iteraci贸n del vector
    		{
-   			b++;																				// Suma uno a la variable J
+   			b++;																			// Suma uno a la variable J
 		}
    		
    }
    
     if(b== 1)																					// Condicional si la variable b esta en uno 
     {
-    	cout<<"El numero que ingresaste se encuentra en el vector.";							// Imprime texto en consola de confirmaci髇 de n鷐ero encontrado
+    	cout<<"El numero que ingresaste se encuentra en el vector.";														// Imprime texto en consola de confirmaci贸n de n煤mero encontrado
     } 
-     else																						// De lo contrario
+     else																					// De lo contrario
      {
-   	  cout<<"El numero que ingresaste no se encuentra en el vector.";							// Imprime texto en consola de negaci髇 de n鷐ero encontrado
+   	  cout<<"El numero que ingresaste no se encuentra en el vector.";													// Imprime texto en consola de negaci贸n de n煤mero encontrado
      }
    
    	
