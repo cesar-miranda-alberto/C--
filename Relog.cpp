7d38869cb671735de5123dc812b14b06c0c88729
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-18 12:46:02 -0600

    Relog.cpp

--- Relog.cpp
+++ Relog.cpp
@@ -1,32 +1,32 @@
 /*
 							***** RELOJ DIGITAL *****
 			
-			Descripción:									
+			DescripciÃ³n:									
 			Programa que implementa un reloj apartir de ciclos FOR
 			Fecha: 24-Febrero-2021
 			Version: 1.0
 	
 */
 
-#include <iostream>															// Biblioteca estandar de salida y entrada												
+#include <iostream>												// Biblioteca estandar de salida y entrada												
 
-using namespace std;														// Acceso a todas la funciones
+using namespace std;												// Acceso a todas la funciones
 
-int main()																	// Función Principal
+int main()													// FunciÃ³n Principal
 {
 	
 
 	
-	for(int i=0; i<24; i++)													// Contador para las horas
+	for(int i=0; i<24; i++)											// Contador para las horas
 	{
-		for(int j=0; j<60; j++)												// Contador para los minutos
+		for(int j=0; j<60; j++)										// Contador para los minutos
 		{
-			for(int k=0; k<60; k++)											// Contador para los segundos
+			for(int k=0; k<60; k++)									// Contador para los segundos
 			{
-				cout<<"HORA: "<<i<<": "<<j<<": "<<k<<"\n";					// Impresión de la hora
+				cout<<"HORA: "<<i<<": "<<j<<": "<<k<<"\n";					// ImpresiÃ³n de la hora
 			}
 		}
 	}
 	
-	return 0;																// Programa finalizado con éxito 
+	return 0;												// Programa finalizado con Ã©xito 
 }
