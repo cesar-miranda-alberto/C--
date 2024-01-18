16cb53873eadf5f94a80e7ffac3f356bb637107f
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-18 12:27:36 -0600

    Calculo_Frecuencia_Cardiaca.cpp

--- Calculo_Frecuencia_Cardiaca.cpp
+++ Calculo_Frecuencia_Cardiaca.cpp
@@ -1,9 +1,9 @@
 /*
 							***** CALCULADORA DE PULSASIONES *****
 		
-		Descripción:
+		DescripciÃ³n:
 		El siguiente programa calcula la frecuencia cardiaca de una persona a partir de la formula
-		220 ó 226 - Edad
+		220 Ã³ 226 - Edad
 		Donde:
 		220 si eres hombre
 		226 si eres mujer
@@ -12,28 +12,28 @@
 		
 */
 
-#include <iostream>																											// Biblioteca estandar de Entrada y Salida de datos
+#include <iostream>																				// Biblioteca estandar de Entrada y Salida de datos
 
-using namespace std;																										// Acceso a todas la funciones
+using namespace std;																				// Acceso a todas la funciones
 
-main()																														// Función principal
+main()																						// FunciÃ³n principal
 {
     
     
-	float edad, pulsam, pulsah;																								// Declaración de variables edad y pulsamh para resultado	
-	cout<<"\t\tPrograma que calcula a frecuencia cardiaca de una persona";													// Impresión de texto en consola con dos tabulaciones
-	cout<<"\n\n";																											// Agrega dos saltos de linea																							// Impresión de opciones en consola conun salto de linea																											// Agrega dos saltos de linea																												// Asignación de valor a la opción
-	cout<<"Ingresa tu edad: ";																								// Requerimiento de edad
-	cin>>edad;																												// Asignación del valor a la variable
+	float edad, pulsam, pulsah;																		// DeclaraciÃ³n de variables edad y pulsamh para resultado	
+	cout<<"\t\tPrograma que calcula a frecuencia cardiaca de una persona";													// ImpresiÃ³n de texto en consola con dos tabulaciones
+	cout<<"\n\n";																				// Agrega dos saltos de linea																							// ImpresiÃ³n de opciones en consola conun salto de linea																											// Agrega dos saltos de linea																												// AsignaciÃ³n de valor a la opciÃ³n
+	cout<<"Ingresa tu edad: ";																		// Requerimiento de edad
+	cin>>edad;																				// AsignaciÃ³n del valor a la variable
 		
-	pulsam=(226-edad);																								        // Calculo para frecuencia cardiaca de una mujer
+	pulsam=(226-edad);																			// Calculo para frecuencia cardiaca de una mujer
 	
-	pulsah=(220-edad);     																						        	// Calculo para frecuencia cardiaca de un hombre
+	pulsah=(220-edad);     																			// Calculo para frecuencia cardiaca de un hombre
 	     																									
-	cout<<"\n\n";																											// Agrega tres saltos de linea desde el ultimo texto impreo  
-	cout<<"La frecuencia cardiaca para una persona con edad de "<<edad<<" es de:";											// Impresión del resultado
+	cout<<"\n\n";																				// Agrega tres saltos de linea desde el ultimo texto impreo  
+	cout<<"La frecuencia cardiaca para una persona con edad de "<<edad<<" es de:";												// ImpresiÃ³n del resultado
 	cout<<"\nMujer: "<<pulsam;
 	cout<<"\nHombre: "<<pulsah;
-	return 0;																												// Programa terminado con éxito
+	return 0;																												// Programa terminado con Ã©xito
 }
 
