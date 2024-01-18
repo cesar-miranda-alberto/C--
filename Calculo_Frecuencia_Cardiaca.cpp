8b0f03feaf1d7d3dc0cd2301efdbad8bf54b990a
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-18 12:25:06 -0600

    Add files via upload

---
+++ Calculo_Frecuencia_Cardiaca.cpp
@@ -0,0 +1,39 @@
+/*
+							***** CALCULADORA DE PULSASIONES *****
+		
+		Descripción:
+		El siguiente programa calcula la frecuencia cardiaca de una persona a partir de la formula
+		220 ó 226 - Edad
+		Donde:
+		220 si eres hombre
+		226 si eres mujer
+		Fecha: 05-Febrero-2021
+		Version: 1.0
+		
+*/
+
+#include <iostream>																											// Biblioteca estandar de Entrada y Salida de datos
+
+using namespace std;																										// Acceso a todas la funciones
+
+main()																														// Función principal
+{
+    
+    
+	float edad, pulsam, pulsah;																								// Declaración de variables edad y pulsamh para resultado	
+	cout<<"\t\tPrograma que calcula a frecuencia cardiaca de una persona";													// Impresión de texto en consola con dos tabulaciones
+	cout<<"\n\n";																											// Agrega dos saltos de linea																							// Impresión de opciones en consola conun salto de linea																											// Agrega dos saltos de linea																												// Asignación de valor a la opción
+	cout<<"Ingresa tu edad: ";																								// Requerimiento de edad
+	cin>>edad;																												// Asignación del valor a la variable
+		
+	pulsam=(226-edad);																								        // Calculo para frecuencia cardiaca de una mujer
+	
+	pulsah=(220-edad);     																						        	// Calculo para frecuencia cardiaca de un hombre
+	     																									
+	cout<<"\n\n";																											// Agrega tres saltos de linea desde el ultimo texto impreo  
+	cout<<"La frecuencia cardiaca para una persona con edad de "<<edad<<" es de:";											// Impresión del resultado
+	cout<<"\nMujer: "<<pulsam;
+	cout<<"\nHombre: "<<pulsah;
+	return 0;																												// Programa terminado con éxito
+}
+
---
+++ Calculo_Perimetro_Triangulo.cpp
@@ -0,0 +1,31 @@
+/*
+		***** PERIMETRO DE UN TRIANGULO *****
+	
+	Descripción:
+	El presente código calcula el área de un triangulo partiendo de los valores
+	dados por el usuario siguiendo la fórmula 
+	P= A+B+C
+*/
+
+#include <iostream>																						// Biblioteca estandar de entrada y salida de los datos
+
+using namespace std;																					// Acceso a las funciones
+
+main()																									// Función principal
+{
+	
+	float Cady, Cop, Hipo, Are;																			// Declaración de variables
+	
+	cout<<"\t\tprograma que calcula el perimetro de un triangulo rectangulo";							// Imprime texto de titulo 
+	cout<<"\n\n";																						// Agrega dos saltos de linea
+	cout<<"Escribe el valor del cateto adyacente: ";													// Toma de requerimientos
+	cin>>Cady;																							// Asignación de valor a la variable cady
+	cout<<"Escribe el valor del cateto opuesto: ";														// Toma de requerimientos
+	cin>>Cop;																							// Asignación de valor a la variable cop
+	cout<<"Escribe el valor de la hipotenusa: ";														// Toma de requerimiento
+	cin>>Hipo;																							// Asignación de valor a la variable hipotenusa
+	Are=Cady+Cop+Hipo;																					// Calculo de área 
+	cout<<"El perimetro del triagulo rectangulo es: "<<Are;												// Imprensión del resultado en consola
+	
+	return 0;
+}
